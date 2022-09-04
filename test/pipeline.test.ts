8cca8da27bcb0b66b5e8c250652ae26d85650875
Indrajit Pingale <indrajit.pingale@panion.org>
2022-09-04 15:40:33 +0200

    Add pipeline test step

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -4,16 +4,13 @@ import * as Pipeline from "../lib/pipeline-stack";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/pipeline-stack.ts
-test("SQS Queue Created", () => {
+test("Empty Stack", () => {
   const app = new cdk.App();
   // WHEN
   const stack = new Pipeline.PipelineStack(app, "MyTestStack");
   // THEN
   const template = Template.fromStack(stack);
-
-  expect(template).toBeInstanceOf(cdk.assertions.Template);
-
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+  template.templateMatches({
+    Resources: undefined,
+  });
 });
