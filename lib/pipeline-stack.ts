5e5777611a09e6cbc8bacdf9d839e5bc4ea65976
Indrajit Pingale <indrajit.pingale@panion.org>
2022-09-04 17:20:02 +0200

    Restart pipeline on update

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -20,6 +20,7 @@ export class PipelineStack extends cdk.Stack {
     const pipeline = new Pipeline(this, "Pipeline", {
       pipelineName: "Pipeline",
       crossAccountKeys: false,
+      restartExecutionOnUpdate: true,
     });
 
     const cdkSourceOutput = new Artifact("cdkSourceOutput");
