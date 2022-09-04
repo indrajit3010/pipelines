347b103a2715070c628e048b631999287f10e5ca
Indrajit Pingale <indrajit.pingale@panion.org>
2022-09-04 15:55:55 +0200

    Generate unit test reports

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  testEnvironment: "node",
+  roots: ["<rootDir>/test"],
+  testMatch: ["**/*.test.ts"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "test-reports",
+        outputName: "test-report.xml",
+      },
+    ],
+  ],
 };
